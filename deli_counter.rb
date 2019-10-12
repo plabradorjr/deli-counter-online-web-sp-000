721ffa733acbdf9f010a59199e41f0f274224910
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 16:00:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here.
-def line(array)
-  if array.length == 0
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
 
