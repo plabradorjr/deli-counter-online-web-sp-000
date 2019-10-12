78dee3467cbfb64c828689796a2d27941cd19809
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 16:19:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{deli.first}."
+    deli.shift
   end
+  line(deli)
 end
