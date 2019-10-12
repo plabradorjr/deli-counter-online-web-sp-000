1f779fe5644aab31b4c4f3d02b5ad671c05a7347
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 15:36:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(array)
+  if array.length == 0
     puts "The line is currently empty."
   else
     puts "The line is currently: "
