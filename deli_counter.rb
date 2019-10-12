6773c9d5fb35f904ae7e5a3244993d20ae662fde
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 16:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number (deli, customer_name)
   deli << customer_name
-  puts "Welcome #{customer_name}. You are number #{deli.length} in line."
+  puts "Welcome, #{customer_name}. You are number #{deli.length} in line."
 end
 
 
